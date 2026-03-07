e67ad433734b5290a2ce4a8c20f80c336ae9e021
Sheetal012345 <thikadesheetal03@gmail.com>
2026-03-07 23:24:48 +0530

    Update version message in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub using Jenkins!");
-        System.out.println("Updated newwwwwwwwwwwwwww version!");
+        System.out.println("Updated newwwwwwwwwwwwwww2222222222222 version!");
     }
 }
