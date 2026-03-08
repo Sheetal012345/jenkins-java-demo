5661a02a54fd22e83fb9f9cbbc80522acd93e915
Sheetal012345 <thikadesheetal03@gmail.com>
2026-03-08 21:54:24 +0530

    Fix typo in Jenkins version message

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub using Jenkins!");
-        System.out.println("Updated newwwwwwwwJenkinsss version!");
+        System.out.println("Updated newwwwwwwwJenkinsssss version!");
     }
 }
